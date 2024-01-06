c26a54eefa98c0694e07de6f17ecea5cd3c0798e
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-01-06 09:19:09 -0300

    add site config file

---
+++ config/site.ts
@@ -0,0 +1,6 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: "Forms | Machado & Sarter advogados associados",
+  description: "Formulário de atendimento de demandas previdenciárias.",
+};
