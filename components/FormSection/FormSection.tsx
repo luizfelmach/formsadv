e31029fa635668880982017fa970e1d24d584019
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-01-06 21:08:49 -0300

    update style

--- components/FormSection/FormSection.tsx
+++ components/FormSection/FormSection.tsx
@@ -1,3 +1,87 @@
+"use client";
+
+import { ArrowRight, ChevronLeft, Loader2, Send } from "lucide-react";
+import { Button } from "../ui/button";
+import { FormsPages } from "@/config/site";
+import { FormProvider, useForm } from "react-hook-form";
+import { GenericInput } from "../GenericInput/GenericInput";
+import { useState } from "react";
+
 export function FormSection() {
-  return <div>ok</div>;
+  const [currentPage, setCurrentPage] = useState<number>(0);
+  const methods = useForm();
+  const { handleSubmit, control, formState } = methods;
+  const { isSubmitting } = formState;
+
+  const submit = async (data: any) => {
+    console.log(data);
+  };
+
+  return (
+    <main className="flex flex-col">
+      <div>
+        <Button
+          variant="outline"
+          size="icon"
+          disabled={currentPage === 0}
+          onClick={() => setCurrentPage(currentPage - 1)}
+        >
+          <ChevronLeft />
+        </Button>
+      </div>
+
+      <header className="my-8">
+        <h1 className="scroll-m-20 text-5xl font-extrabold tracking-tight lg:text-5xl">
+          {FormsPages[currentPage].title}
+        </h1>
+        <h2 className="scroll-m-20 border-b pb-2 text-3xl font-normal tracking-tight first:mt-0">
+          {FormsPages[currentPage].subtitle}
+        </h2>
+      </header>
+
+      <FormProvider {...methods}>
+        <form onSubmit={handleSubmit(submit)} className="space-y-4">
+          {FormsPages.map((form, formIndex) =>
+            form.inputs.map((input, inputIndex) => (
+              <div
+                key={inputIndex}
+                className={formIndex !== currentPage ? "hidden" : ""}
+              >
+                <GenericInput
+                  control={control}
+                  defaultValue={""}
+                  inputProps={input}
+                />
+              </div>
+            ))
+          )}
+
+          {currentPage + 1 === FormsPages.length && (
+            <div className="pt-32">
+              <Button type="submit" disabled={isSubmitting} className="w-full">
+                {isSubmitting ? (
+                  <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                ) : (
+                  <Send className="mr-2 h-4 w-4" />
+                )}
+                Finalizar
+              </Button>
+            </div>
+          )}
+        </form>
+      </FormProvider>
+
+      {currentPage + 1 != FormsPages.length && (
+        <div className="self-end my-10">
+          <Button
+            variant="default"
+            size="icon"
+            onClick={() => setCurrentPage(currentPage + 1)}
+          >
+            <ArrowRight />
+          </Button>
+        </div>
+      )}
+    </main>
+  );
 }
--- config/site.ts
+++ config/site.ts
@@ -1,6 +1,114 @@
+import { z } from "zod";
+import { FormPageType } from "@/types";
+
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
   name: "Forms | Machado & Sarter advogados associados",
   description: "Formulário de atendimento de demandas previdenciárias.",
 };
+
+export const FormsPages: FormPageType[] = [
+  {
+    title: "Olá, precisamos conhecer melhor você!",
+    subtitle: "Informe seu nome completo no campo abaixo.",
+    inputs: [
+      {
+        inputKey: "nome",
+        type: "text",
+        label: "Nome",
+        isRequired: true,
+        validation: z.string().min(1, "Insira seu nome."),
+      },
+      {
+        inputKey: "easteregg",
+        type: "text",
+        label: "Easter Egg",
+        isRequired: false,
+        validation: z.string().min(1, "Insira seu nome."),
+      },
+      {
+        inputKey: "teste",
+        type: "text",
+        label: "teste",
+        isRequired: false,
+        validation: z.string().min(1, "Insira seu nome."),
+      },
+      {
+        inputKey: "teste1",
+        type: "text",
+        label: "teste1",
+        isRequired: false,
+        validation: z.string().min(1, "Insira seu nome."),
+      },
+      {
+        inputKey: "teste2",
+        type: "date",
+        label: "teste2",
+        placeholder: "Escolha uma data",
+        isRequired: false,
+        validation: z.string().min(1, "Insira seu nome."),
+      },
+
+      {
+        inputKey: "teste3",
+        type: "checkbox",
+        label: "teste3",
+        placeholder: "Escolha uma data",
+        isRequired: false,
+        validation: z.string().min(1, "Insira seu nome."),
+        options: [
+          {
+            value: "option 1",
+          },
+          {
+            value: "Option 2",
+          },
+          {
+            value: "Option 3",
+          },
+          {
+            value: "Option 4",
+          },
+        ],
+      },
+
+      {
+        inputKey: "teste4",
+        type: "radio",
+        label: "teste4",
+        placeholder: "Escolha uma data",
+        isRequired: false,
+        validation: z.string().min(1, "Insira seu nome."),
+        options: [
+          {
+            value: "option 1",
+          },
+          {
+            value: "Option 2",
+          },
+          {
+            value: "Option 3",
+          },
+          {
+            value: "Option 4",
+          },
+        ],
+      },
+    ],
+  },
+
+  {
+    title: "Digite seu CPF!",
+    subtitle: "Insira somente os números.",
+    inputs: [
+      {
+        inputKey: "cpf",
+        type: "text",
+        label: "CPF",
+        isRequired: true,
+        validation: z.string(),
+      },
+    ],
+  },
+];
