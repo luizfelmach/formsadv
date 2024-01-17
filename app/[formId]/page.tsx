c9a9e4ffe4d04b1daab79440482e51a57d4ef95b
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-01-17 13:00:19 -0300

    update

---
+++ app/[formId]/page.tsx
@@ -0,0 +1,21 @@
+import { FormSection } from "@/components/FormSection/FormSection";
+import { notFound } from "next/navigation";
+
+type paramsProps = {
+  formId: string;
+};
+
+interface PageProps {
+  params: paramsProps;
+}
+
+export default async function Page({ params }: PageProps) {
+  const { formId } = params;
+  const form = null;
+  if (!form) {
+    notFound();
+  }
+  return (
+    <main className="container mx-auto max-w-2xl pt-16 px-6 flex-grow">OK</main>
+  );
+}
