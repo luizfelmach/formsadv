d7c93a040eca2b1a217f46edc5621be57501a9bc
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-01-17 23:46:16 -0300

    update

--- config/site.ts
+++ config/site.ts
@@ -4,8 +4,8 @@ import { generateId } from "@/lib/utils";
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Forms | Machado & Sarter advogados associados",
-  description: "Formulário de atendimento de demandas previdenciárias.",
+  name: "Meu Form",
+  description: "Crie formulários personalizados.",
 };
 
 const save: FormPageEntity[] = [
