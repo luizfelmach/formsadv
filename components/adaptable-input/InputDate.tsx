7e704831b8c79e0e5202c92c509d23999bc19943
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-01-24 09:39:23 -0300

    update

--- components/adaptable-input/InputDate.tsx
+++ components/adaptable-input/InputDate.tsx
@@ -1,22 +1,12 @@
 import { UseControllerProps } from "react-hook-form";
-import { Button } from "@/components/ui/button";
-import { CalendarIcon } from "lucide-react";
-import { Calendar } from "@/components/ui/calendar";
-import { cn } from "@/lib/utils";
-import { format } from "date-fns";
-import { ptBR } from "date-fns/locale";
 import {
   FormControl,
   FormField,
   FormItem,
   FormMessage,
 } from "@/components/ui/form";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import { ScreenType } from "../types";
+import { Input } from "../ui/input";
 
 interface GenericInputProps extends UseControllerProps {
   screen: ScreenType;
@@ -24,7 +14,7 @@ interface GenericInputProps extends UseControllerProps {
 
 export function InputDate(props: Omit<GenericInputProps, "name">) {
   const { screen, control, defaultValue } = props;
-  const { screenKey } = screen;
+  const { screenKey, type } = screen;
   return (
     <FormField
       defaultValue={defaultValue}
@@ -32,40 +22,19 @@ export function InputDate(props: Omit<GenericInputProps, "name">) {
       name={screenKey}
       render={({ field }) => (
         <FormItem className="flex flex-col">
-          <Popover>
-            <PopoverTrigger asChild>
-              <FormControl>
-                <Button
-                  variant={"outline"}
-                  style={{
-                    boxShadow: "none",
-                    borderTop: "none",
-                    borderLeft: "none",
-                    borderRight: "none",
-                  }}
-                  className="w-full border-b-4 h-14 bg-accent focus:border-foreground/20 transition-colors"
-                >
-                  {field.value ? (
-                    format(field.value, "PPP", { locale: ptBR })
-                  ) : (
-                    <span></span>
-                  )}
-                  <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
-                </Button>
-              </FormControl>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0" align="start">
-              <Calendar
-                locale={ptBR}
-                mode="single"
-                selected={field.value}
-                onSelect={(e) => {
-                  field.onChange(e);
-                }}
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
+          <FormControl>
+            <Input
+              style={{
+                boxShadow: "none",
+                borderTop: "none",
+                borderLeft: "none",
+                borderRight: "none",
+              }}
+              className="w-full border-b-4 h-14 bg-accent focus:border-foreground/20 transition-colors"
+              type={type}
+              {...field}
+            />
+          </FormControl>
           <FormMessage />
         </FormItem>
       )}
