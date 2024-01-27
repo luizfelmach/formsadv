28fe76a16a24f15c65fd3c04b208561c69cddf3d
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-01-27 20:22:45 -0300

    update

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,14 +1,25 @@
+"use client";
 import { NotFoundSvg } from "@/components/svg/not-found";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/navigation";
 
 export default function NotFound() {
+  const router = useRouter();
   return (
     <div className="bg-background justify-center items-center h-screen mx-8 flex flex-col-reverse lg:flex-row">
       <div>
         <h2 className="scroll-m-20 pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           Não foi possível encontrar essa página.
         </h2>
-        <Button className="mt-4">Página inicial</Button>
+        <Button
+          type="button"
+          className="mt-4"
+          onClick={() => {
+            router.back();
+          }}
+        >
+          Voltar
+        </Button>
       </div>
       <NotFoundSvg />
     </div>
