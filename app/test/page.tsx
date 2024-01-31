c3c4327a7c5a314e78377674d06a35cd92bf7096
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-01-31 11:21:37 -0300

    add inline input

---
+++ app/test/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import { InputInline } from "@/components/module/inline-input";
+import { useForm } from "react-hook-form";
+
+export default function Page() {
+  const { handleSubmit, setValue, getValues } = useForm<{
+    test: string;
+  }>({
+    defaultValues: {
+      test: "luiz",
+    },
+  });
+  return (
+    <form onSubmit={handleSubmit((data) => console.log(data))}>
+      <InputInline
+        className="text-over scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl "
+        onInput={(e) => {
+          setValue("test", e.currentTarget.innerText);
+        }}
+        preventEnter
+        value={getValues("test")}
+      />
+      <input type="text" />
+      <button type="submit">Enviar</button>
+    </form>
+  );
+}
