1878697f860be749bb761b80a1f0e6fe84fa5245
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-02-02 10:16:46 -0300

    update

--- components/interface/reply-box.tsx
+++ components/interface/reply-box.tsx
@@ -1,5 +1,8 @@
 import { cn } from "@/lib/utils";
 import { Container } from "../container";
+import { Button } from "../ui/button";
+import { ChevronLeft, Loader, MoveRight } from "lucide-react";
+import { HTMLAttributes } from "react";
 
 interface ReplyBoxProps {
   children?: React.ReactNode;
@@ -8,14 +11,16 @@ interface ReplyBoxProps {
 
 function ReplyBoxRoot({ children, className }: ReplyBoxProps) {
   return (
-    <Container
-      className={cn(
-        "min-h-screen flex flex-col justify-center items-center",
-        className
-      )}
-    >
-      <div className="w-full">{children}</div>
-    </Container>
+    <div className="overflow-hidden">
+      <Container
+        className={cn(
+          "min-h-screen flex flex-col justify-center items-center",
+          className
+        )}
+      >
+        <div className="w-full space-y-8">{children}</div>
+      </Container>
+    </div>
   );
 }
 
@@ -24,7 +29,7 @@ interface ReplyBoxHeaderProps {
 }
 
 function ReplyBoxHeader({ children }: ReplyBoxHeaderProps) {
-  return <header className="my-8 space-y-4">{children}</header>;
+  return <header className="my-8 mx-1 space-y-4">{children}</header>;
 }
 
 interface ReplyBoxTitleProps {
@@ -56,7 +61,7 @@ interface ReplyBoxInputProps {
 }
 
 function ReplyBoxInput({ children }: ReplyBoxInputProps) {
-  return <div>{children}</div>;
+  return <div className="m-1">{children}</div>;
 }
 
 interface ReplyBoxFooterProps {
@@ -64,23 +69,71 @@ interface ReplyBoxFooterProps {
 }
 
 function ReplyBoxFooter({ children }: ReplyBoxFooterProps) {
-  return <div>{children}</div>;
+  return <div className="mx-1">{children}</div>;
 }
 
-interface ReplyBoxHandleNextProps {
-  children?: React.ReactNode;
+interface ReplyBoxHandleNextProps extends HTMLAttributes<HTMLButtonElement> {
+  active?: boolean;
 }
 
-function ReplyBoxHandleNext({ children }: ReplyBoxHandleNextProps) {
-  return <div>{children}</div>;
+function ReplyBoxHandleNext({ active, ...props }: ReplyBoxHandleNextProps) {
+  if (!active) return null;
+  return (
+    <div className="flex justify-end mx-1">
+      <Button className="h-11 font-bold" {...props}>
+        <div className="flex justify-between w-32 items-center">
+          <span>Continuar</span>
+          <span>
+            <MoveRight />
+          </span>
+        </div>
+      </Button>
+    </div>
+  );
 }
 
-interface ReplyBoxHandleBackProps {
-  children?: React.ReactNode;
+interface ReplyBoxHandleCompleteProps
+  extends HTMLAttributes<HTMLButtonElement> {
+  active?: boolean;
+  isSubmitting?: boolean;
 }
 
-function ReplyBoxHandleBack({ children }: ReplyBoxHandleBackProps) {
-  return <div>{children}</div>;
+function ReplyBoxHandleComplete({
+  active,
+  isSubmitting,
+  ...props
+}: ReplyBoxHandleCompleteProps) {
+  if (!active) return null;
+  return (
+    <Button
+      className="w-full h-11 font-bold mt-8"
+      type="submit"
+      variant={"default"}
+      disabled={isSubmitting}
+      {...props}
+    >
+      Finalizar
+      {isSubmitting && <Loader className="animate-spin" />}
+    </Button>
+  );
+}
+
+interface ReplyBoxHandleBackProps extends HTMLAttributes<HTMLButtonElement> {
+  active?: boolean;
+}
+
+function ReplyBoxHandleBack({ active, ...props }: ReplyBoxHandleBackProps) {
+  if (!active) return null;
+  return (
+    <Button
+      {...props}
+      className="bg-accent text-accent-foreground hover:bg-foreground/10 mx-1"
+      size="icon"
+      type="button"
+    >
+      <ChevronLeft />
+    </Button>
+  );
 }
 
 export const ReplyBox = {
@@ -92,4 +145,5 @@ export const ReplyBox = {
   Footer: ReplyBoxFooter,
   HandleNext: ReplyBoxHandleNext,
   HandleBack: ReplyBoxHandleBack,
+  HandleComplete: ReplyBoxHandleComplete,
 };
--- components/reply/index.tsx
+++ components/reply/index.tsx
@@ -1,14 +1,11 @@
 "use client";
 
-import { ChevronLeft, ChevronRight, Loader, MoveRight } from "lucide-react";
-import { Container } from "../container";
 import { FormType } from "../../types";
-import { Button } from "../ui/button";
 import { useReplyForm } from "./hooks";
-import { ReplyFormHeader } from "./components/reply-form-header";
 import { FormProvider } from "react-hook-form";
 import { RevealSlide } from "../reveal-slide";
 import { ReplyInput } from "./components/reply-input";
+import { ReplyBox } from "../interface/reply-box";
 
 interface ReplyFormProps {
   form: FormType;
@@ -41,97 +38,50 @@ export function ReplyForm({ form }: ReplyFormProps) {
   }
 
   return (
-    <Container className="min-h-screen flex flex-col justify-center items-center">
-      {canGoBack && (
-        <section className="w-full flex justify-start px-4">
-          <Button
-            className="bg-accent text-accent-foreground hover:bg-foreground/10"
-            size="icon"
-            type="button"
-            onClick={() => handleBack()}
-          >
-            <ChevronLeft />
-          </Button>
-        </section>
-      )}
+    <ReplyBox.Root>
+      <ReplyBox.HandleBack active={canGoBack} onClick={() => handleBack()} />
 
       {!completed && (
         <FormProvider {...methods}>
-          <form
-            onSubmit={methods.handleSubmit(handleSubmit)}
-            className="w-full overflow-hidden"
-          >
+          <form onSubmit={methods.handleSubmit(handleSubmit)}>
             {screens.map((screen, index) => (
               <RevealSlide
                 key={index}
                 visible={index === screenIndex}
                 direction={index > screenIndex}
               >
-                <div className="w-full">
-                  <section className="w-full">
-                    <Container className="w-full">
-                      <ReplyFormHeader
-                        title={screen.title}
-                        description={screen.description}
-                      />
-                    </Container>
-                  </section>
-                  <Container className="w-full">
-                    <ReplyInput screen={screen} handleNext={handleNext} />
-                  </Container>
-                </div>
+                <ReplyBox.Header>
+                  <ReplyBox.Title>{screen.title}</ReplyBox.Title>
+                  <ReplyBox.Description>
+                    {screen.description}
+                  </ReplyBox.Description>
+                </ReplyBox.Header>
+                <ReplyBox.Input>
+                  <ReplyInput screen={screen} handleNext={handleNext} />
+                </ReplyBox.Input>
               </RevealSlide>
             ))}
-
-            <section className="w-full">
-              <Container className="w-full">
-                {canComplete && (
-                  <Button
-                    className="w-full h-11 font-bold"
-                    type="submit"
-                    variant={"default"}
-                    disabled={isSubmitting}
-                  >
-                    Finalizar
-                    {isSubmitting && <Loader className="animate-spin" />}
-                  </Button>
-                )}
-              </Container>
-            </section>
+            <ReplyBox.HandleComplete
+              active={canComplete}
+              isSubmitting={isSubmitting}
+            />
           </form>
         </FormProvider>
       )}
 
       <RevealSlide visible={completed} direction={true}>
-        <div className="w-full">
-          <section className="w-full">
-            <Container className="w-full">
-              <ReplyFormHeader
-                title={form.endScreen.title}
-                description={form.endScreen.description}
-              />
-            </Container>
-          </section>
-        </div>
+        <ReplyBox.Header>
+          <ReplyBox.Title>{form.endScreen.title}</ReplyBox.Title>
+          <ReplyBox.Description>
+            {form.endScreen.description}
+          </ReplyBox.Description>
+        </ReplyBox.Header>
       </RevealSlide>
 
-      {canProceed && (
-        <section className="w-full flex justify-end px-4">
-          <Button
-            onClick={async () => {
-              handleNext();
-            }}
-            className="h-11 font-bold"
-          >
-            <div className="flex justify-between w-32 items-center">
-              <span>Continuar</span>
-              <span>
-                <MoveRight />
-              </span>
-            </div>
-          </Button>
-        </section>
-      )}
-    </Container>
+      <ReplyBox.HandleNext
+        active={canProceed}
+        onClick={async () => handleNext()}
+      />
+    </ReplyBox.Root>
   );
 }
