265a944ecf4dcd36a6f796273ce2b48f248abb72
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2024-02-02 11:09:02 -0300

    update

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -1,4 +1,10 @@
+import { Metadata } from "next";
 import { Auth } from "@/components/pages/auth";
+
+export const metadata: Metadata = {
+  title: "Meu Form | Autenticação",
+  description: "Crie uma conta ou acesse a plataforma Meu Forms.",
+};
 
 export default function AuthRoute() {
   return <Auth />;
--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,12 @@
+import { Metadata } from "next";
 import { isAuthenticated } from "@/actions";
 import { Dashboard } from "@/components/pages/dashboard";
 import { redirect } from "next/navigation";
+
+export const metadata: Metadata = {
+  title: "Meu Form | Dashboard",
+  description: "Gerencie seus formulários.",
+};
 
 export default async function DashboardRoute() {
   //if (!(await isAuthenticated())) {
